3442a18d8b6db9439d518c27590614c93d3b2388
Sam Willis <samuel.j.willis@outlook.com>
2017-09-10 17:04:01 +0100

    only runs a single instance now

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-# this does nothing
-from time import sleep
+#/usr/bin/env python
 
-while True:
-    print("test")
-    sleep(1)
+import os
+import sys
+import atexit
+from test import runforever
+
+pid = str(os.getpid())
+pidfile = "/tmp/remote-python-code.pid"
+
+if os.path.isfile(pidfile):
+    print("{0} already exists, exiting".format(pidfile))
+    sys.exit()
+
+with open(pidfile, 'w') as f:
+    atexit.register(lambda: os.unlink(pidfile))
+    f.write(pid)
+
+runforever()
---
+++ test.py
@@ -0,0 +1,8 @@
+# this does nothing
+from time import sleep
+
+
+def runforever():
+    while True:
+        print("test")
+        sleep(1)
